5a3df2347a1c0ba3b74503e03f55c2091f2cd0fb
globalsmile <mohammedbadmus823@gmail.com>
2024-01-04 09:00:48 +0000

    fix: 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,9 @@
 export default function createIteratorObject(report) {
-	while (report) {
-		console.log('Placeholder')
+	let itr = report.allEmployees;
+	let itr2 = Object.values(itr);
+	let itr3 = [];
+	for (let idx in itr2) {
+		itr3.push(...itr2[idx]);
 	}
+	return itr3;
 }
