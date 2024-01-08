51e0ae1fc5b39a0febe1529ee81141e37ee7e640
globalsmile <mohammedbadmus823@gmail.com>
2024-01-08 21:33:46 +0100

    add: 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function get_list_students() {
+  return [
+    {id: 1, firstName: 'Guaillaume', location: 'San Francisco'},
+    {id: 2, firstName: 'James', location: 'Columbia'},
+    {id: 3, firstName: 'Serena', location: 'San Francisco'}
+  ];
+};
