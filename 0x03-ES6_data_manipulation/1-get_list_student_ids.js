3a98fca8a14b8aaa97195aa061b759d9bdae5e07
globalsmile <mohammedbadmus823@gmail.com>
2024-01-08 23:26:23 +0000

    fix: array type of 1-get_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(getListStudents) {
-  if (typeof (getListStudents) !== 'object') return [];
+  if (!Array.isArray(getListStudents)) return [];
   const arrayOfIds = getListStudents.map(({ id }) => id);
   return arrayOfIds;
 }
