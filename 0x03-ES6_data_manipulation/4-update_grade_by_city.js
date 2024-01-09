aa625e58ea2e7d1d2a87a625c83e40448a100e8f
globalsmile <mohammedbadmus823@gmail.com>
2024-01-09 14:52:35 +0100

    add: 4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  const listStudents = getListStudents();
+  const students = listStudents.filter((student) => student.location === city);
+  return students.map((student) => {
+    const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id)[0];
+    if (grade) {
+      return { ...student, grade: grade.grade };
+    }
+    return { ...student, grade: 'N/A' };
+  });
+}
