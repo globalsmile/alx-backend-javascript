72454526bf7901b89f69d92bc8858790035b7ed3
globalsmile <mohammedbadmus823@gmail.com>
2024-03-01 09:30:18 +0100

    modified: 5-http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,19 +2,21 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
-  if (req.url === '/students') {
-    res.write('This is the list of our students\n');
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     try {
       const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write('This is the list of our students\n');
+      res.end(data.join('\n'));
     } catch (error) {
-      res.end(error.message);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(`This is the list of our students\n${error.message}`);
     }
   }
-  res.end();
-});
+}
+);
 app.listen(1245);
 module.exports = app;
